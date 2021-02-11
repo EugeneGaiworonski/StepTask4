cafccb61a68509f586076541e4567f6d5b793b7a
Eugene Gaiworonski <78734970+EugeneGaiworonski@users.noreply.github.com>
2021-02-11 21:25:16 +0300

    Create prg2.cpp

---
+++ prg2.cpp
@@ -0,0 +1,40 @@
+// Задание 2
+// Напишите программу, которая создает двухмерный массив и заполняет его
+// по следующему принципу: пользователь вводит число (например, 3)
+// первый элемент массива принимает значение этого числа, последующий
+// элемент массива принимает значение этого числа + 1
+// (т.е. 6 для нашего примера), третий элемент массива предыдущий элемент + 1
+// (т.е. 6*2=12 для нашего примера).
+// Созданный массив вывести на экран.
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+ int length = 0;
+ int number = 0;
+ cout<<"\t3Array length?= ";cin>>length;
+
+ int (*array)[3] = new int[length][3];
+
+ for(int i = 0; i < length; i++)
+   {
+     std::cout<<"\tNumber?="; std::cin>>number;
+     array[i][0] = number;
+     array[i][1] = array[i][0] + 1;
+     array[i][2] = array[i][1] + 1;
+   }
+  for (int i = 0; i < length; i++)
+     {
+       for (int j = 0; j < 3; j++)
+         {
+           std::cout<<"\t["<<i<<"]["<<j<<"]= "<<array[i][j];
+         }
+       cout << '\n';
+     }
+
+  delete[] array;
+  array = 0;
+  return 0;
+}
